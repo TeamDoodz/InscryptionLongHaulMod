1064fc673ddb106552aa1b18a8c5478eedbde3d2
TeamDoodz <unconfigured@null.spigotmc.org>
2021-12-05 16:59:38 -0800

    setup configs

---
+++ InscryptionLongHaulMod/scripts/BattleModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongHaulMod {
+	class BattleModule : ModuleBase {
+
+		public BattleModule(MainPlugin plugin) : base(plugin) { }
+
+	}
+}
